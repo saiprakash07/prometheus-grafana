3dbf81e247119c2b023ef8ed9c2bd9b2c16e1741
saiprakash07 <88754277+saiprakash07@users.noreply.github.com>
2022-05-02 23:38:12 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "helm_release" "prometheus_grafana" {
+  name       = "prometheus"
+  namespace  = "monitoring"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  timeout    = 6000
+}
